31894f9bfd6d7b6354a550ff2c484808c8895490
ramanjaneyulu17 <ramanjaneyulu17.m@gmail.com>
2025-01-09 00:14:03 +0530

    no changes

--- src/main/java/com/developer/bookStore/repository/BooksRepo.java
+++ src/main/java/com/developer/bookStore/repository/BooksRepo.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BooksRepo extends JpaRepository<Books,Integer> {
-
 }
--- src/main/java/com/developer/bookStore/service/BooksService.java
+++ src/main/java/com/developer/bookStore/service/BooksService.java
@@ -3,9 +3,6 @@ package com.developer.bookStore.service;
 import com.developer.bookStore.model.Books;
 import com.developer.bookStore.repository.BooksRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
